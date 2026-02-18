f328fe4f8207c96a97ba7478dd3bf8eab82aed9f
nuoen <nuoenzhang@outlook.com>
2026-02-19 00:23:54 +0800

    libbpfstrace: decode common syscall arguments

--- libbpfstrace/libbpfstrace.c
+++ libbpfstrace/libbpfstrace.c
@@ -1,5 +1,6 @@
 #include <dirent.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,6 +29,75 @@ static int libbpf_print_fn(enum libbpf_print_level level, const char *fmt,
     return vfprintf(stderr, fmt, args);
 }
 
+static const char *futex_op_name(unsigned long long op)
+{
+    switch (op & 0x7f) {
+    case 0:
+        return "FUTEX_WAIT";
+    case 1:
+        return "FUTEX_WAKE";
+    case 2:
+        return "FUTEX_FD";
+    case 3:
+        return "FUTEX_REQUEUE";
+    case 4:
+        return "FUTEX_CMP_REQUEUE";
+    case 5:
+        return "FUTEX_WAKE_OP";
+    case 6:
+        return "FUTEX_LOCK_PI";
+    case 7:
+        return "FUTEX_UNLOCK_PI";
+    case 8:
+        return "FUTEX_TRYLOCK_PI";
+    case 9:
+        return "FUTEX_WAIT_BITSET";
+    case 10:
+        return "FUTEX_WAKE_BITSET";
+    case 11:
+        return "FUTEX_WAIT_REQUEUE_PI";
+    case 12:
+        return "FUTEX_CMP_REQUEUE_PI";
+    case 13:
+        return "FUTEX_LOCK_PI2";
+    default:
+        return "FUTEX_UNKNOWN";
+    }
+}
+
+static void format_openat_flags(unsigned long long flags, char *buf, size_t sz)
+{
+    size_t off = 0;
+    unsigned long long access = flags & O_ACCMODE;
+
+    if (access == O_RDONLY)
+        off += snprintf(buf + off, sz - off, "O_RDONLY");
+    else if (access == O_WRONLY)
+        off += snprintf(buf + off, sz - off, "O_WRONLY");
+    else if (access == O_RDWR)
+        off += snprintf(buf + off, sz - off, "O_RDWR");
+    else
+        off += snprintf(buf + off, sz - off, "0x%llx", access);
+
+#define APPEND_FLAG(f) \
+    do { \
+        if ((flags) & (f)) \
+            off += snprintf(buf + off, sz - off, "|%s", #f); \
+    } while (0)
+    APPEND_FLAG(O_CREAT);
+    APPEND_FLAG(O_EXCL);
+    APPEND_FLAG(O_NOCTTY);
+    APPEND_FLAG(O_TRUNC);
+    APPEND_FLAG(O_APPEND);
+    APPEND_FLAG(O_NONBLOCK);
+    APPEND_FLAG(O_DSYNC);
+    APPEND_FLAG(O_SYNC);
+    APPEND_FLAG(O_CLOEXEC);
+    APPEND_FLAG(O_DIRECTORY);
+    APPEND_FLAG(O_NOFOLLOW);
+#undef APPEND_FLAG
+}
+
 static const char *syscall_name(int nr)
 {
     switch (nr) {
@@ -45,10 +115,54 @@ static const char *syscall_name(int nr)
     }
 }
 
+static void format_syscall_args(const struct syscall_event *e, const char *name, char *out,
+                                size_t out_sz)
+{
+    switch (e->syscall_id) {
+    case __NR_read:
+        snprintf(out, out_sz, "%s(fd=%llu, buf=%#llx, count=%llu)",
+                 name, e->args[0], e->args[1], e->args[2]);
+        break;
+    case __NR_write:
+        snprintf(out, out_sz, "%s(fd=%llu, buf=%#llx, count=%llu)",
+                 name, e->args[0], e->args[1], e->args[2]);
+        break;
+    case __NR_ioctl:
+        snprintf(out, out_sz, "%s(fd=%llu, cmd=%#llx, arg=%#llx)",
+                 name, e->args[0], e->args[1], e->args[2]);
+        break;
+    case __NR_futex:
+        snprintf(out, out_sz,
+                 "%s(uaddr=%#llx, op=%s(%#llx), val=%llu, timeout/uaddr2=%#llx, val3=%#llx)",
+                 name, e->args[0], futex_op_name(e->args[1]), e->args[1], e->args[2], e->args[3],
+                 e->args[4]);
+        break;
+    case __NR_openat: {
+        char flagbuf[256];
+        format_openat_flags(e->args[2], flagbuf, sizeof(flagbuf));
+        snprintf(out, out_sz,
+                 "%s(dfd=%lld, pathname=%#llx, flags=%s, mode=%#llo)",
+                 name, (long long)e->args[0], e->args[1], flagbuf, e->args[3]);
+        break;
+    }
+    case __NR_epoll_pwait:
+        snprintf(out, out_sz,
+                 "%s(epfd=%llu, events=%#llx, maxevents=%llu, timeout=%lld, sigmask=%#llx, sigsetsize=%llu)",
+                 name, e->args[0], e->args[1], e->args[2], (long long)e->args[3], e->args[4],
+                 e->args[5]);
+        break;
+    default:
+        snprintf(out, out_sz, "%s(%#llx, %#llx, %#llx, %#llx, %#llx, %#llx)",
+                 name, e->args[0], e->args[1], e->args[2], e->args[3], e->args[4], e->args[5]);
+        break;
+    }
+}
+
 static int handle_event(void *ctx, void *data, size_t data_sz)
 {
     const struct syscall_event *e = data;
     const char *name;
+    char callbuf[512];
 
     (void)ctx;
     if (data_sz < sizeof(*e))
@@ -59,12 +173,9 @@ static int handle_event(void *ctx, void *data, size_t data_sz)
     if (strncmp(name, "sys_", 4) == 0)
         name += 4;
 
-    printf("pid=%d tid=%d comm=%s %s(%#llx, %#llx, %#llx, %#llx, %#llx, %#llx) [id=%d]\n",
-           e->tgid, e->pid, e->comm,
-           name,
-           e->args[0], e->args[1], e->args[2],
-           e->args[3], e->args[4], e->args[5],
-           e->syscall_id);
+    format_syscall_args(e, name, callbuf, sizeof(callbuf));
+    printf("pid=%d tid=%d comm=%s %s [id=%d]\n",
+           e->tgid, e->pid, e->comm, callbuf, e->syscall_id);
     return 0;
 }
 
