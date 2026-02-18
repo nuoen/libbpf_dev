f82f1b062f940606ad805f1575321de99582becb
nuoen <nuoenzhang@outlook.com>
2026-02-19 00:01:33 +0800

    libbpfstrace: require pid or process name and wait by name

--- libbpfstrace/libbpfstrace.c
+++ libbpfstrace/libbpfstrace.c
@@ -1,3 +1,4 @@
+#include <dirent.h>
 #include <errno.h>
 #include <signal.h>
 #include <stdio.h>
@@ -55,23 +56,88 @@ static int parse_pid(const char *s, int *pid)
     return 0;
 }
 
+static int read_proc_comm(int pid, char *buf, size_t buf_sz)
+{
+    char path[64];
+    FILE *fp;
+
+    snprintf(path, sizeof(path), "/proc/%d/comm", pid);
+    fp = fopen(path, "r");
+    if (!fp)
+        return -1;
+
+    if (!fgets(buf, (int)buf_sz, fp)) {
+        fclose(fp);
+        return -1;
+    }
+    fclose(fp);
+
+    buf[strcspn(buf, "\n")] = '\0';
+    return 0;
+}
+
+static int find_pid_by_name(const char *name, int *pid)
+{
+    DIR *dir;
+    struct dirent *de;
+    char *end = NULL;
+    char comm[256];
+
+    dir = opendir("/proc");
+    if (!dir)
+        return -1;
+
+    while ((de = readdir(dir)) != NULL) {
+        long v;
+
+        errno = 0;
+        v = strtol(de->d_name, &end, 10);
+        if (errno || end == de->d_name || *end != '\0' || v <= 0 || v > 0x7fffffff)
+            continue;
+
+        if (read_proc_comm((int)v, comm, sizeof(comm)) < 0)
+            continue;
+        if (strcmp(comm, name) == 0) {
+            *pid = (int)v;
+            closedir(dir);
+            return 0;
+        }
+    }
+
+    closedir(dir);
+    return -1;
+}
+
 int main(int argc, char **argv)
 {
     struct libbpfstrace_bpf *skel = NULL;
     struct ring_buffer *rb = NULL;
     int target_pid = 0;
     int err;
+    int is_pid = 0;
 
-    if (argc > 2) {
-        fprintf(stderr, "Usage: %s [pid]\n", argv[0]);
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <pid|process_name>\n", argv[0]);
         return 1;
     }
 
-    if (argc == 2) {
-        if (parse_pid(argv[1], &target_pid) < 0) {
-            fprintf(stderr, "invalid pid: %s\n", argv[1]);
+    signal(SIGINT, sig_handler);
+    signal(SIGTERM, sig_handler);
+
+    if (parse_pid(argv[1], &target_pid) == 0) {
+        is_pid = 1;
+    } else {
+        printf("waiting for process name '%s' ...\n", argv[1]);
+        while (!exiting) {
+            if (find_pid_by_name(argv[1], &target_pid) == 0)
+                break;
+            sleep(1);
+        }
+        if (exiting || target_pid <= 0) {
+            fprintf(stderr, "interrupted before process '%s' started\n", argv[1]);
             return 1;
         }
+        printf("found process '%s' with pid=%d\n", argv[1], target_pid);
     }
 
     libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
@@ -104,13 +170,10 @@ int main(int argc, char **argv)
         goto cleanup;
     }
 
-    signal(SIGINT, sig_handler);
-    signal(SIGTERM, sig_handler);
-
-    if (target_pid)
-        printf("libbpfstrace started, filtering pid=%d\n", target_pid);
+    if (is_pid)
+        printf("libbpfstrace started, tracing pid=%d\n", target_pid);
     else
-        printf("libbpfstrace started, tracing all processes\n");
+        printf("libbpfstrace started, tracing process '%s' pid=%d\n", argv[1], target_pid);
 
     while (!exiting) {
         err = ring_buffer__poll(rb, 200);
